1c265c96ec00877e9e424749a2f57d4def215256
Juyeon <46835524+onlycoca@users.noreply.github.com>
2022-03-29 14:55:40 +0900

    Update on GitHub

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ print("Hello World")
 #adding new item in Hotfix
 print("Hello World 2")
 
-#adding new item in Hotfix
+#adding new item in master branch
 print("Hello World 3")
+
 
 #adding new item in Hotfix
 print("Hello World 4")
 
 #adding new item in master branch
 print("Hello World 5")
+
+#Updates on mainbranch of Github
+print("Hello World Updates on GitHub")
